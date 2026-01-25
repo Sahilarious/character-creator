c39c5eb6ade30874c8ca4308a21ad0dc2c553a9b
Denise Castellanos <castden88@gmail.com>
2026-01-24 21:44:38 -0800

    installed pynput to make updating stat values interactive, added listener for adding/subtracting values and made is asynchronous

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import msvcrt
+import sys
+
+
+import threading
+import keyboard
+import pynput
+from pynput.keyboard import Key, Controller
+from enum import Enum
+
+class Stats(Enum):
+    NONE = 0
+    Strength = 1
+    Dexterity = 2
+    Constitution = 3
+    Intelligence = 4
+    Wisdom = 5
+    Charisma = 6
+
+controller = Controller()
+
 startingPointsPerStat = 8
 maximumTotalPoints = 20 + 6 * startingPointsPerStat
 currentPointsAvailable = maximumTotalPoints
@@ -11,6 +32,8 @@ constitution = startingPointsPerStat
 intelligence = startingPointsPerStat
 wisdom = startingPointsPerStat
 charisma = startingPointsPerStat
+
+currentStatUpdate = Stats.NONE
 
 def canAddPoints():
     if (strength + dexterity + constitution + intelligence + wisdom + charisma < maximumTotalPoints):
@@ -26,25 +49,108 @@ def printStats():
     print(f"Wisdom {wisdom}")
     print(f"Charisma {charisma}")
 
+def onPress(key):
+
+    try:
+
+        #print(f"{key.char}")
+
+        global strength
+        global dexterity
+        global constitution
+        global intelligence
+        global wisdom
+        global charisma
+
+
+        if currentStatUpdate != Stats.NONE and (key.char == 'a' or key.char == 'd'):
+             if (currentStatUpdate == Stats.Strength):
+                if key.char == 'a':
+                    if strength > 1:
+                        strength -= 1
+                        print(f"{strength}")
+                elif key.char == 'd':
+                     if strength < maximumPointsPerStat:
+                        strength += 1
+                        print(f"{strength}")
+             elif (currentStatUpdate == Stats.Dexterity):
+                 if key.char == 'a':
+                    if dexterity > 1:
+                        dexterity -= 1
+                        print(f"{dexterity}")
+                 elif key.char == 'd':
+                     if dexterity < maximumPointsPerStat:
+                        dexterity += 1
+                        print(f"{dexterity}")
+
+             elif (currentStatUpdate == Stats.Constitution):
+                    if key.char == 'a':
+                        if constitution > 1:
+                            constitution -= 1
+                            print(f"{constitution}")
+                    elif key.char == 'd':
+                        if constitution < maximumPointsPerStat:
+                            constitution += 1
+                            print(f"{constitution}")
+
+             elif (currentStatUpdate == Stats.Intelligence):
+                  if key.char == 'a':
+                        if intelligence > 1:
+                            intelligence -= 1
+                            print(f"{intelligence}")
+                  elif key.char == 'd':
+                     if intelligence < maximumPointsPerStat:
+                        intelligence += 1
+                        print(f"{intelligence}")
+
+             elif (currentStatUpdate == Stats.Wisdom):
+                    if key.char == 'a':
+                        if wisdom > 1:
+                            wisdom -= 1
+                            print(f"{wisdom}")
+                    elif key.char == 'd':
+                         if wisdom < maximumPointsPerStat:
+                             wisdom += 1
+                             print(f"{wisdom}")
+    
+             elif (currentStatUpdate == Stats.Charisma):
+                     if key.char == 'a':
+                        if charisma > 1:
+                            charisma -= 1
+                            print(f"{charisma}")
+                     elif key.char == 'd':
+                        if charisma < maximumPointsPerStat:
+                            charisma += 1
+                            print(f"{charisma}")
+
+    except AttributeError:
+        pass
+
+
 def updateStat(startValue):
-    selection = input()
+    #selection = input()
 
     while(selection == 'a' or selection == 'd'):
         if selection == 'a':
             if startValue > 1:
                 startValue -= 1
                 print(f"{startValue}")
+                #keyboard.wait('a')
         elif selection == 'd':
             if startValue < maximumPointsPerStat:
                 startValue += 1
                 print(f"{startValue}")
-        selection = input()
+                #keyboard.wait('d')
+        #selection = input()
+        #keyboard.press(Key.enter)
 
     return startValue
 
 def start():
     global strength
     global dexterity
+    global currentStatUpdate
+
     print("Enter your character name")
     characterName = input()
     print(f"{characterName} has been created")
@@ -54,17 +160,42 @@ def start():
 
     print(f"Strength: {strength}")
 
-    strength = updateStat(strength)
+    currentStatUpdate = Stats.Strength
+
+    keyboard.wait("space")
+
+
 
     print("Set dexterity - a: subtract d: add")
 
+    currentStatUpdate = Stats.Dexterity
+
+
     print(f"Dexterity: {dexterity}")
 
-    dexterity = updateStat(dexterity)
+    keyboard.wait("space")
+
+
+    #dexterity = updateStat(dexterity)
 
     printStats()
 
+def clear_keyboard_buffer():
+    while msvcrt.kbhit():
+        msvcrt.getch()
+
+#listeners
+#with keyboard.Listener(on_press=onPress) as listener:
+ #   listener.join() 
+
+listener = pynput.keyboard.Listener(on_press=onPress)
+listener.start()
 
 
-
+# Start program
 start()
+
+clear_keyboard_buffer()
+
+print("Buffer cleared. Program terminating.")
+sys.exit()
